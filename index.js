0c55db9e5a87ce93ded8cbf606d1f3c1941a4147
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-07-20 10:22:36 +0000

    work

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(currentTime) {
+  const hour =  parseInt("11:24".split(':')[0], 10);
+
+  if (hour < 12) {
+    return "Good Morning";
+  } else {
+    if (hour < 17) {
+      return "Good Afternoon";
+    }
+  } else {
+    return "Good Evening";
+  }
+  
+}
 /* Write your implementation of displayMessage() */
